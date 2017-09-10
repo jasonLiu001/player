55cfce0fe8dd2eac4e4f487fc4be70df446f65a9
wangliu <wangliu@isoftstone.com>
2017-09-10 14:18:39 +0800

    init

---
+++ player/player/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace player
+{
+    class Program
+    {
+        /// <summary>
+        /// 文件路径
+        /// </summary>
+        public static string filePath = ConfigurationManager.AppSettings["filePath"].ToString();
+
+        /// <summary>
+        /// 计时器
+        /// </summary>
+        private static System.Timers.Timer aTimer = new System.Timers.Timer();
+
+        /// <summary>
+        /// 上次的计划文字
+        /// </summary>
+        private static string lastPlanText = string.Empty;
+
+        static void Main(string[] args)
+        {
+            //设置计时器
+            //SetTimer();
+            GetPlanText();
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// 获取计划字符串
+        /// </summary>
+        /// <returns></returns>
+        private static string GetPlanText()
+        {
+            var txt = string.Empty;
+            var titleText = GetWindowCaptionTitle();
+            var txtArray = titleText.Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+
+            for (var i = 0; i < txtArray.Length; i++)
+            {
+                var itemText = txtArray[i];
+                if (itemText.Contains("计划进行"))
+                {
+                    txt = itemText;
+                    break;
+                }
+            }
+            return txt;
+        }
+
+        /// <summary>
+        /// 获取窗口标题文字
+        /// </summary>
+        /// <returns></returns>
+        private static string GetWindowCaptionTitle()
+        {
+            IntPtr maindHwnd = Win32.FindWindow("WTWindow", null); //获取窗口句柄
+            if (maindHwnd == IntPtr.Zero)
+            {
+                Console.WriteLine("未找到对应的窗口");
+                return string.Empty;
+            }
+
+            //第一个子窗口
+            IntPtr firstChildWin = Win32.FindWindowEx(maindHwnd, IntPtr.Zero, "Edit", null);  //第一个子窗口
+            //计划窗口 maindHwnd为主窗口 表示在这里面查找 如果替换成子窗口，说明在子窗口中查找
+            IntPtr planWin = Win32.FindWindowEx(maindHwnd, firstChildWin, "Edit", null);  //计划窗口
+            int maxLength = 1000000;
+
+            IntPtr buffer = Marshal.AllocHGlobal((maxLength + 1) * 2);
+            Win32.SendMessageW2(planWin, Constant.WM_GETTEXT, (uint)maxLength, buffer);
+            string windowCaptionTitle = Marshal.PtrToStringUni(buffer);
+            return windowCaptionTitle;
+        }
+
+
+        /// <summary>
+        /// 设置计时器
+        /// </summary>
+        private static void SetTimer()
+        {
+            aTimer.Elapsed += new ElapsedEventHandler(TimeEvent);
+            // 设置引发时间的时间间隔 此处设置为１秒（１０００毫秒）
+            aTimer.Interval = 10000;
+            aTimer.Enabled = true;
+        }
+
+        /// <summary>
+        /// 计时器定时触发事件
+        /// </summary>
+        private static void TimeEvent(object sender, ElapsedEventArgs e)
+        {
+            //首次
+            if (string.IsNullOrEmpty(lastPlanText))
+            {
+                var titleText = GetWindowCaptionTitle();
+                //保存文本
+                lastPlanText = titleText;
+                //保存到文件
+                Console.WriteLine(titleText);
+                return;
+            }
+
+            var newTitleText = GetWindowCaptionTitle();
+            if (newTitleText == lastPlanText) return; //计划未更新
+
+            //保存新计划文本
+            lastPlanText = newTitleText;
+            //保存到文件
+            Console.WriteLine(newTitleText);
+        }
+    }
+}
---
+++ player/player/common/Win32.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace player
+{
+    static class Win32
+    {
+        [DllImport("user32.dll")]
+
+        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [System.Runtime.InteropServices.DllImportAttribute("user32.dll", EntryPoint = "SendMessageW")]
+        public static extern int SendMessageW2([System.Runtime.InteropServices.InAttribute()] System.IntPtr hWnd, uint Msg, uint wParam, IntPtr lParam);
+
+
+        [DllImport("User32.dll", EntryPoint = "SendMessage")]
+
+        private static extern int SendMessage(IntPtr hWnd, int Msg, IntPtr wParam, string lParam);
+
+        [DllImport("User32.dll")]
+
+        public static extern IntPtr FindWindowEx(IntPtr hwndParent, IntPtr hwndChildAfter, string strclass, string FrmText);
+    }
+}
